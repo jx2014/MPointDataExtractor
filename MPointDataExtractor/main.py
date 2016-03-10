56b4b865d5256b56a8870a0b7d327e700767d84a
JX_ULTRA <jx@xj.com>
2016-03-10 00:03:37 -0800

    add main.py
    Signed-off-by: JX_ULTRA <jx@xj.com>

---
+++ MPointDataExtractor/main.py
@@ -0,0 +1,24 @@
+import DataExtractor as DE
+import MpointHTMLExtractor as HE
+
+#dataDir = r"C:\Users\jxue\Documents\Projects_LocalDrive\Bridge2.0\"
+#BLTData = "MPoint_data1.txt"
+
+datafile = r"C:\Users\jxue\Documents\Projects_LocalDrive\Bridge2.0\mPoint.txt"
+htmlfile = r"C:\Users\jxue\Documents\Projects_LocalDrive\Gen5_Qualification\Test Results for 0013500700000880.html"
+
+MpointData = 0 # basic .txt data
+MpointHTML = 1 # get mpoint data from html page with complete tests
+
+if MpointData == 1:
+    BLT = DE.MPointDataExtractor(datafile)
+    BLT.LineProcess()
+    
+    for i in BLT.listOfMacNMsg:
+        print i
+    
+    BLT.WriteCSV()
+
+elif MpointHTML == 1:
+    BLT = HE.MPointHtmlExtractor(htmlfile)
+    BLT.Test()
